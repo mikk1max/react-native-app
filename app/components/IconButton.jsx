50fcefaa8e551c3dd9ecd2a4f029f8dbf275f6f2
Maksym Shepeta <maksim.shepeta@gmail.com>
2024-10-08 02:56:53 +0200

    feat: add categories
    fix: some errors

---
+++ app/components/IconButton.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+import { View, StyleSheet, TouchableOpacity, Text } from "react-native";
+import FontAwesome6 from "@expo/vector-icons/FontAwesome6";
+
+const IconButton = ({ iconName, onPress, containerWidth, isActive }) => {
+  // Obliczanie rozmiaru przycisku
+  const buttonSize = (containerWidth - 10 * 4) / 5; // 10 to odstęp między przyciskami
+
+  const backgroundColor = isActive ? "#000" : "#D3D3D3";
+  const iconColor = isActive ? "#D3D3D3" : "#000";
+
+  return (
+    <View style={styles.container(buttonSize)}>
+      <TouchableOpacity
+        style={[styles.iconBtn, { backgroundColor }]}
+        onPress={onPress}
+      >
+        <FontAwesome6 name={iconName} size={25} color={iconColor} />
+      </TouchableOpacity>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: (size) => ({
+    width: size,
+    height: size,
+    justifyContent: "center",
+    alignItems: "center",
+  }),
+  iconBtn: {
+    width: "100%",
+    height: "100%",
+    justifyContent: "center",
+    alignItems: "center",
+    borderRadius: "100%",
+  },
+});
+
+export default IconButton;
--- app/components/SearchBar.jsx
+++ app/components/SearchBar.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import { SearchBar } from "react-native-elements";
+import { StyleSheet } from "react-native";
 
 const SearchBarComp = ({ onSearch }) => {
   const [search, setSearch] = useState("");
@@ -11,28 +12,41 @@ const SearchBarComp = ({ onSearch }) => {
 
   return (
     <SearchBar
-      placeholder="Type Here..."
+      placeholder="Search"
       onChangeText={updateSearch}
       value={search}
       round={true}
-      containerStyle={{
-        backgroundColor: "transparent",
-        borderTopColor: "transparent",
-        borderBottomColor: "transparent",
-        paddingHorizontal: 0,
-        height: 45,
-        padding: 0,
-        margin: 0,
-      }}
-      inputContainerStyle={{
-        width: "100%",
-        height: 45,
-        backgroundColor: "#D3D3D3",
-        paddingHorizontal: 10,
-        justifyContent: "center",
-      }}
+      containerStyle={styles.container}
+      inputContainerStyle={styles.inputContainer}
+      inputStyle={styles.input}
+      searchIcon={{ size: 25, color: "#161616" }}
+      clearIcon={{ size: 25, color: "#161616" }}
     />
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: "transparent",
+    borderTopColor: "transparent",
+    borderBottomColor: "transparent",
+    paddingHorizontal: 0,
+    height: 45,
+    padding: 0,
+    margin: 0,
+  },
+  inputContainer: {
+    width: "100%",
+    height: 45,
+    backgroundColor: "#D3D3D3",
+    paddingHorizontal: 10,
+    justifyContent: "center",
+  },
+  input: {
+    fontFamily: "Work Sans",
+    fontSize: 16,
+    color: "#323f4b", // Set text color if needed
+  },
+});
 
 export default SearchBarComp;
